48da5836a63e91c6624e0f4c06bc728f1a7eb355
matt.lanuza09 <matt.lanuza09@gmail.com>
2024-12-02 19:52:28 +0800

    add create post routes and controllers

--- routes/post.js
+++ routes/post.js
@@ -8,5 +8,20 @@ const router = express.Router();
 // Create new blog post (authenticated users only)
 router.post('/createPost', verify, userController.createPost);
 
+// Get all posts (all users)
+
+// Get a post by ID (all users)
+
+// Update a post by ID (authenticated user)
+
+// Delete a post by ID (authenticated user)
+
+// Delete any post by ID (admin user)
+
+// Delete any comments by ID (admin user)
+
+// Add comment on a post by ID (authenticated user)
+
+// Get all comments on a post by ID (authenticated user)
 
 module.exports = router;

bff55d5fe555f112dff2ffd8bed9cbf7faf51bce
matt.lanuza09 <matt.lanuza09@gmail.com>
2024-12-02 18:29:53 +0800

    update index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// Routes Middleware
+const userRoutes = require("./routes/user");
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
+const corsOptions = {
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+
+//[Database Connection]
+const MONGODB_STRING = "mongodb+srv://admin:admin123@wdc028-b461.qepkz.mongodb.net/Blog-App-API?retryWrites=true&w=majority&appName=WDC028-B461";
+mongoose.connect(MONGODB_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open',()=>console.log("Now connected to MongoDB Atlas"));
+
+//[Backend Routes]
+// app.use("/users", userRoutes);
+
+
+
+const PORT = 4000;
+if(require.main === module){
+	app.listen(PORT, () => {
+	    console.log(`API is now online on port ${PORT}`)
+	});
+}
+
+module.exports = {app,mongoose};

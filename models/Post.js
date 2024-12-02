3aca896c7066db05611165508c6d17efd1dde44b
matt.lanuza09 <matt.lanuza09@gmail.com>
2024-12-02 18:41:42 +0800

    add models

---
+++ models/Post.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+});
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Title is required'],
+  },
+  content: {
+    type: String,
+    required: [true, 'Content is required'],
+  },
+  author: {
+    type: String,
+    required: [true, 'Author information is required'],
+  },
+  creationDate: {
+    type: Date,
+    default: Date.now,
+  },
+  comments: [commentSchema],
+});
+
+module.exports = mongoose.model("Post", postSchema);

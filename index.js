455e1dd289ad46ad4f2ce18b2bfd17714a94edeb
matt.lanuza09 <matt.lanuza09@gmail.com>
2024-12-02 19:00:03 +0800

    add user routes and controllers register

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
 const corsOptions = {
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://blog-post-server.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
